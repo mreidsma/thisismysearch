0e2e2f8ee41d354e9e979d0c379ebec31430972b
Matthew Reidsma <reidsmam@gvsu.edu>
2015-05-14 13:49:52 -0400

    Added Lansing Community College Summon

--- systems.php
+++ systems.php
@@ -17,7 +17,8 @@ $systems = array(
 				"http://digital.library.villanova.edu/Search/Results?lookfor=",
                 "http://iucat.iu.edu/?utf8=%E2%9C%93&search_field=all_fields&q=",
 				"http://catalogue.library.carleton.ca/search/?searchtype=X&SORT=D&searcharg=",
-				"http://carletonu.summon.serialssolutions.com/#!/search?ho=t&q="
+				"http://carletonu.summon.serialssolutions.com/#!/search?ho=t&q=",
+				"http://lcc.summon.serialssolutions.com/#!/search?ho=t&q="
 				
 			);
 			
